9a9b5407248a3e47d7d30ce4e6dca7b679add844
guillaumevaillant59 <vaillantguillaume@yahoo.fr>
2026-03-18 14:29:42 +0100

    ajout model partie

--- src/app/models/partie.model.ts
+++ src/app/models/partie.model.ts
@@ -1,5 +1,12 @@
-export default interface Partie {
-    id:number,
-    
+import Carte from "./carte.model";
+import DomaineReine from "./domaineReine.model";
+import Joueur from "./joueur.model";
 
+export default interface Partie {
+    id : number,
+    domaineReine : DomaineReine,
+    pioche : Array<Carte>,
+    joueurs : Array<Joueur>,
+    nombreMaxJoueur : number,
+    status : string
 }
