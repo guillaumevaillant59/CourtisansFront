041537d40d7729ce6a7a54b5c8a9621c1ec08700
guillaumevaillant59 <vaillantguillaume@yahoo.fr>
2026-03-18 12:00:14 +0100

    debut front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Acceuil } from './pages/acceuil/acceuil';
+import { Parties } from './pages/parties/parties';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "", component : Acceuil},
+    {path : "parties", component : Parties}
+];
---
+++ src/app/pages/parties/parties.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parties',
+  imports: [],
+  templateUrl: './parties.html',
+  styleUrl: './parties.css',
+})
+export class Parties {}
